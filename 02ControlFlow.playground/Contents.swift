1715fb840a0f81ff74e4357fb7639157305f508f
simyoju <dawnofspring@daum.net>
2021-08-19 10:44:42 +0900

    create a file

--- 02ControlFlow.playground/Contents.swift
+++ 02ControlFlow.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let adult = 19
+let age = 15
+
+/*
+ if는 swift 키워드 이다.
+ swift는 if문 뒤에 괄호를 쳐도 되고, 안쳐도 된다.
+ */
+
+if age < adult {
+    print("당신은 미성년자 입니다. ")
+}
